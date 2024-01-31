3eb92580025870194747ac9a881d645582e733f8
Jingye Xu <jingye.xu@outlook.com>
2024-01-31 16:44:43 -0600

    add support for rfid mqtt

---
+++ RFID_RC522/rfid_pi/rfid_read_mqtt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+from paho.mqtt import client as mqtt_client
+
+# mqtt info
+broker_ip = "192.168.0.101"
+broker_port = 1883
+topic = "test/rfid"
+client_id = "rfid_pi"
+
+# connect to mqtt broker
+client = mqtt_client.Client(client_id)
+client.connect(broker_ip, broker_port)
+
+reader = SimpleMFRC522()
+
+try:
+    print("Ctrl + C to exit")
+    while True:
+        id, text = reader.read()
+        print(f"UID: \t{id}")
+        print(f"Content: \t{text}\n")
+        client.publish(topic, f"{id}-{text}")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
