62da19a45034dbe1eb196a7cd72032727ecb8edb
Jingye Xu <jingye.xu@outlook.com>
2024-02-26 17:05:33 -0600

    code update support config file

---
+++ utils/record_config.py
@@ -0,0 +1,8 @@
+# this config file is used to generate works
+from data_record import Sensor
+
+# format:
+# "port": ["filename", Sensor.type]
+sensors_info = {
+    # "port1": ["filename1", Sensor.tof_single],
+}
