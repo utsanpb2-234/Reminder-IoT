b7ddd39adf55015228f7fb0a359eaf2917370174
Jingye Xu <jingye.xu@outlook.com>
2024-03-04 18:05:13 -0600

    code update

--- utils/all_in_one.py
+++ utils/all_in_one.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from multiprocessing import Process
 from case_record import caseRecord
 from data_record import dataRecord, Sensor
 import time
@@ -9,7 +10,7 @@ if __name__ == "__main__":
 
     for sensor_port in sensors_info.keys():
         sensor = dataRecord(sensor_port, sensors_info[sensor_port][0], sensors_info[sensor_port][1])
-        sensor_thread = Thread(target=sensor.run, daemon=True)
+        sensor_thread = Process(target=sensor.run, daemon=True)
         sensor_thread.start()
 
     case1 = caseRecord("case1.csv")
